5969324dbc8c828540c98357ce19b7ce0e4a678d
Imon-Ovbude <imonhimio@gmail.com>
2022-02-23 19:31:49 -0800

    Review project: added funtionality to select random person to show review

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -31,7 +31,13 @@ const Review = () => {
     });
   };
 
-  const randomPerson = () => {};
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
 
   return (
     <article className='review'>
@@ -52,7 +58,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className='random-btn'>surprise me</button>
+      <button className='random-btn' onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
