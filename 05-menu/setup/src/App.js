36b29209c96e77447ada3d5907aab6a1eb383933
Imon-Ovbude <imonhimio@gmail.com>
2022-02-28 16:21:54 -0800

    created filter category function and category state and passed to categories.js

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,21 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map(item => item.category))];
+
 function App() {
-  const [menuItems, setMenu] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = category => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter(item => item.category === category);
+
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -15,7 +27,7 @@ function App() {
           <div className='underline'></div>
         </div>
 
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu menuItems={menuItems} />
       </section>
     </main>
